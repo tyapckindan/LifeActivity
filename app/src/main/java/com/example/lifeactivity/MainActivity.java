d8c6988fe92b0244fcf65e639760db9c509695e7
Данила Тяпкин <dtapkin6@gmail.com>
2023-03-06 17:27:39 +0300

    Исправил

--- app/src/main/java/com/example/lifeactivity/MainActivity.java
+++ app/src/main/java/com/example/lifeactivity/MainActivity.java
@@ -1,13 +1,14 @@
 package com.example.lifeactivity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
-
     private final static String TAG = "MainActivity";
 
     @Override
@@ -24,45 +25,55 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onDestroy(){
+    protected void onDestroy() {
         super.onDestroy();
         Log.d(TAG, "onDestroy");
         Toast.makeText(this, "onDestroy()", Toast.LENGTH_LONG).show();
     }
+
     @Override
-    protected void onStop(){
+    protected void onStop() {
         super.onStop();
         Log.d(TAG, "onStop");
         Toast.makeText(this, "onStop()", Toast.LENGTH_LONG).show();
     }
+
     @Override
-    protected void onStart(){
+    protected void onStart() {
         super.onStart();
         Log.d(TAG, "onStart");
         Toast.makeText(this, "onStart()", Toast.LENGTH_LONG).show();
+        resetUI();
     }
+
     @Override
-    public void onSaveInstanceState(Bundle savedInstanceState) {
+    public void onSaveInstanceState(@NonNull Bundle savedInstanceState) {
         super.onSaveInstanceState(savedInstanceState);
     }
 
     @Override
-    protected void onPause(){
+    protected void onPause() {
         super.onPause();
         Log.d(TAG, "onPause");
         Toast.makeText(this, "onPause()", Toast.LENGTH_LONG).show();
     }
+
     @Override
-    protected void onResume(){
+    protected void onResume() {
         super.onResume();
         Log.d(TAG, "onResume");
         Toast.makeText(this, "onResume()", Toast.LENGTH_LONG).show();
     }
 
     @Override
-    protected void onRestart(){
+    protected void onRestart() {
         super.onRestart();
         Log.d(TAG, "onRestart");
         Toast.makeText(this, "onRestart()", Toast.LENGTH_LONG).show();
+    }
+    private void resetUI() {
+        int count = 5;
+        ((TextView) findViewById(R.id.txt_counter)).setText(Integer.toString(count));
+        Log.d(TAG, "resetUI");
     }
 }
